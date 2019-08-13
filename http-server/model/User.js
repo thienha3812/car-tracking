7248432be98202abd787d1929ec1c6135e7a568d
Macbook <macbook@Macbooks-MacBook-Pro.local>
2019-08-13 10:49:33 +0700

    update database

--- http-server/model/User.js
+++ http-server/model/User.js
@@ -15,11 +15,20 @@ var UserSchema = new Schema({
         required : true,
         trim : true
     },
+    email : {
+        type : String,
+        required:true,
+        trim : true
+    },
+    fullName : {
+        type : String,        
+        required : true
+    },
     role:{
         type : Number,
-        required : true,        
+        required : true,  
+        default : 3      
     }
-
 })
 
 module.exports = mongoose.model('User',UserSchema)
--- http-server/routes/user.js
+++ http-server/routes/user.js
@@ -8,4 +8,20 @@ router.post('/login',function(req,res,next){
         console.log(result)
     })
 })
+router.post('/register',function(req,res,next){
+    const user = {
+        username : req.body.username,
+        password : req.body.password,
+        fullName : req.body.fullName,
+        email : req.body.email,        
+    }
+    User.create(user).then((result)=>{
+        console.log(result)
+    }).catch(err=>{
+        res.statusCode(500)
+    })
+})
+router.post('/logout',function(){
+
+})
 module.exports = router
