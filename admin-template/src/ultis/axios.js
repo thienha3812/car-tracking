66a862e90251b3d76b2c4b96512203d3a1490da5
Macbook <macbook@Macbooks-MacBook-Pro.local>
2019-08-23 08:49:37 +0700

    123

--- admin-template/src/ultis/axios.js
+++ admin-template/src/ultis/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 
 var request = axios.create({
-    baseURL : "http://localhost:5024",
+    baseURL : "http://117.2.128.107:5024",
     timeout : 10000
 })
 request.interceptors.request.use((config)=>{
