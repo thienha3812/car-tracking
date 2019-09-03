25dfc1dec55e2b3498ec9e75f37300bdff01b385
Macbook <macbook@Macbooks-MacBook-Pro.local>
2019-09-03 10:05:29 +0700

    update

--- http-server/routes/drivers.js
+++ http-server/routes/drivers.js
@@ -4,13 +4,13 @@ var Driver = require("../model/Driver");
 var formidable = require("formidable");
 var uuid = require("uuid");
 var boom = require("boom");
-router.get("/getall", function(req, res, next) {
-  Driver.find({}, function(err, result) {
+router.get("/getall", function (req, res, next) {
+  Driver.find({}, function (err, result) {
     if (err) throw err;
     res.json(result);
   });
 });
-router.post("/insert", function(req, res, next) {
+router.post("/insert", function (req, res, next) {
   var fileName = uuid.v4();
   var form = new formidable.IncomingForm();
   var filePath = process.env.IP + "/image/" + fileName;
@@ -34,25 +34,24 @@ router.post("/insert", function(req, res, next) {
         dr_unit: fields["dr_unit"]
       });
       await d.save();
-      // res.sendStatus(200)
     } else {
-      // res.status(500).send({message:"Vui lòng nhập lại thông tin!"})
+      res.status(500).send({ message: "Vui lòng nhập lại thông tin!" })
     }
   });
-  form.on("fileBegin", async   function (name, file)  {
+  form.on("fileBegin", async function (name, file) {
     if (file.type == "image/png") {
-      file.path = "./upload/" + fileName + ".png";            
+      file.path = "./upload/" + fileName + ".png";
     }
     if (file.type == "image/jpg") {
       file.path = "./upload/" + fileName + ".jpg";
     }
   })
-  form.on("end",()=>{
-    res.send("200")
+  form.on("end", () => {
+    res.sendStatus(200)
   })
 });
-router.post("/delete", function(req, res, next) {
-  Driver.findByIdAndRemove({ _id: req.body._id }, function(err, result) {
+router.post("/delete", function (req, res, next) {
+  Driver.findByIdAndRemove({ _id: req.body._id }, function (err, result) {
     if (err) {
       res.status(404).send(err);
     } else {
@@ -60,7 +59,7 @@ router.post("/delete", function(req, res, next) {
     }
   });
 });
-router.post("/update", function(req, res, next) {
+router.post("/update", function (req, res, next) {
   Driver.findOneAndUpdate(
     { _id: req.body._id },
     {
@@ -73,7 +72,7 @@ router.post("/update", function(req, res, next) {
       dr_sex: req.body.dr_sex,
       dr_unit: req.body.dr_unit
     },
-    function(err, result) {
+    function (err, result) {
       if (err) {
         res.status(404).send(err);
       } else {
